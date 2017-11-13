77576f02bed7553205194aa4c3cbf67b50e83a19
zhangxin <zhangxin@zhangxindeMacBook-Pro.local>
2017-11-13 17:11:30 +0800

    修改cnn code

--- tensorflow实现cnn.py
+++ tensorflow实现cnn.py
@@ -28,7 +28,7 @@ def bias_variable(shape):
 
 def conv2d(x, W):
     #卷积遍历各方向步数为1，SAME: 边缘外自动补0，遍历相乘
-    return tf.cnn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
     #池化卷积(conv2d)池化层采用kernel大小为2*2，步数也为2，周围补0，取最大值。数据量缩小了4倍
@@ -36,19 +36,19 @@ def max_pool_2x2(x):
 
 #构建网络
 x_image = tf.reshape(x, [-1,28,28,1]) #转换输入数据shape,恢复到最初形状，以便用于网络中
-W_conval = weight_variable([[5, 5, 1, 32]])
+W_conval = weight_variable([5, 5, 1, 32])
 b_convl = bias_variable([32])
 h_convl = tf.nn.relu(conv2d(x_image, W_conval) + b_convl) #第一个卷积层
-h_pool1 = max_pool(h_convl)                               #第一个池化层
+h_pool1 = max_pool_2x2(h_convl)                               #第一个池化层
 
 w_conv2 = weight_variable([5,5,32,64])
 b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conva2) + b_conv2) #第二个卷积层
-h_pool2 = max_pool(h_conv2)                               #第二个池化层
+h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2) #第二个卷积层
+h_pool2 = max_pool_2x2(h_conv2)                               #第二个池化层
 
 W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape((h_pool2, W_fc1) + b_fc1)      #reshape成 向量空间
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])     #reshape成 向量空间
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)   #第一个全连接层
 
 keep_prob = tf.placeholder("float")
@@ -61,12 +61,14 @@ y_predict = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)    #softmax层
 cross_entropy = -tf.reduce_sum(y_actual*tf.log(y_predict))     #交叉熵
 train_step = tf.train.GradientDescentOptimizer(1e-3).minimize(cross_entropy)    #梯度下降法
 correct_prediction = tf.equal(tf.argmax(y_predict,1), tf.argmax(y_actual,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))                 #精确度计算
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))   #精确度计算
+
+
 sess=tf.InteractiveSession()
-sess.run(tf.initialize_all_variables())
+sess.run(tf.global_variables_initializer())
 for i in range(20000):
     batch = mnist.train.next_batch(50)
-    if i%100 == 0:                  #训练100次，验证一次
+    if i % 100 == 0:                  #训练100次，验证一次
         train_acc = accuracy.eval(feed_dict={x:batch[0], y_actual: batch[1], keep_prob: 1.0})
         print('step %d, training accuracy %g' %(i,train_acc))
         train_step.run(feed_dict={x: batch[0], y_actual: batch[1], keep_prob: 0.5})
